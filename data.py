e2c2f559977c5e061bdef9fe9aa0892a43d83ef2
PiliVeas <pilar.veas@gmail.com>
2024-11-27 09:28:09 -0300

    Update data.py

--- data.py
+++ data.py
@@ -2,6 +2,11 @@ urban_routes_url = 'https://cnt-8f379ad7-194e-4681-8b45-9a088c374ad4.containerhu
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
-card_number = '1234 5678 9100'
-card_code = '111'
+card_info = {
+    "number": "1234 5678 9100",       # Número de tarjeta de prueba
+    "expiry_date": "12/26",           # Fecha de vencimiento
+    "cardholder_name": "John Doe",    # Nombre del titular
+    "cvv": "111"                      # Código de seguridad
+}
+confirmation_code = "123456"
 message_for_driver = 'Muéstrame el camino al museo'
