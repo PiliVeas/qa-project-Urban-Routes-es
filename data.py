7de5846097e8c40c36e6fee1c3f8a585d14bffd0
PiliVeas <pilar.veas@gmail.com>
2024-11-27 09:46:19 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-8f379ad7-194e-4681-8b45-9a088c374ad4.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-76fd0ba9-fbdc-401e-8a76-e6d7ec253ff6.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
