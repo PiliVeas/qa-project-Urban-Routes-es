881836193a4b29f0a4e2a265ed242e1d5b0a3b90
PiliVeas <pilar.veas@gmail.com>
2024-11-28 12:39:57 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,12 +1,9 @@
-urban_routes_url = 'https://cnt-76fd0ba9-fbdc-401e-8a76-e6d7ec253ff6.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-b31b22cc-86dc-4707-8e9f-56483b93bf3b.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
 card_info = {
     "number": "1234 5678 9100",       # Número de tarjeta de prueba
-    "expiry_date": "12/26",           # Fecha de vencimiento
-    "cardholder_name": "John Doe",    # Nombre del titular
     "cvv": "111"                      # Código de seguridad
 }
-confirmation_code = "123456"
 message_for_driver = 'Muéstrame el camino al museo'
